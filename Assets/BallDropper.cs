94a353abaf6411b4eeeead00971e21faef1b0cc7
alzseven <jhp7140@gmail.com>
2023-10-07 20:12:06 +0900

    Show next ball

--- Assets/BallDropper.cs
+++ Assets/BallDropper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class BallDropper : MonoBehaviour
@@ -12,9 +13,11 @@ public class BallDropper : MonoBehaviour
     public float speed;
     private Vector2 inputVec;
     private GameObject currentBall;
-    private float gameTime;
+    private GameObject nextBall;
+    public static float gameTime;
 
     public TMP_Text text;
+    public Image nextBallSprite;
     
     public static int BallCount;
     
@@ -26,16 +29,23 @@ public class BallDropper : MonoBehaviour
 
     private void Start()
     {
+        SetNextBall();
         GetRandomBall();
     }
 
+    void SetNextBall()
+    {
+        nextBall = balls[Random.Range(0, balls.Length)];
+    }
+    
     void GetRandomBall()
     {
-        currentBall = Instantiate(balls[Random.Range(0, balls.Length)]);
+        currentBall = Instantiate(nextBall);
         currentBall.GetComponent<Rigidbody2D>().gravityScale = 0;
         currentBall.GetComponent<Rigidbody2D>().simulated = false;
         currentBall.GetComponent<Ball>().OnBallCollisionEnter2D += OnBallCollisionEnter2D;
         BallCount++;
+        SetNextBall();
     }
     
     // Update is called once per frame
@@ -45,6 +55,11 @@ public class BallDropper : MonoBehaviour
         gameTime += Time.deltaTime;
         transform.position += (Vector3)inputVec * (speed * Time.deltaTime); 
         if(currentBall != null) currentBall.transform.position = transform.position;
+        if (nextBall != null)
+        {
+            nextBallSprite.sprite = nextBall.GetComponent<SpriteRenderer>().sprite;
+            nextBallSprite.color = nextBall.GetComponent<SpriteRenderer>().color;
+        }
     }
 
     private void OnMove(InputValue value)
