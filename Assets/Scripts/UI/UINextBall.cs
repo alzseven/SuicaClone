9d075a64ac67c017a3dae047b7dfba128625f9df
alzseven <jhp7140@gmail.com>
2023-11-13 04:00:30 +0900

    Bind UI and static variables
    - NextBall Image
    - Score Text

---
+++ Assets/Scripts/UI/UINextBall.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Data;
+using Data.Variables;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Image))]
+    public class UINextBall : MonoBehaviour
+    {
+        [SerializeField] private ObservableIntValue nextBallIndex;
+        [SerializeField] private EveryBallData everyBallData;
+        private Image _nextBallImage;
+        
+        private void Awake() => _nextBallImage = GetComponent<Image>();
+
+        private void OnEnable() => nextBallIndex.OnValueChanged += OnNextBallIndexChanged;
+
+        private void OnDisable() => nextBallIndex.OnValueChanged -= OnNextBallIndexChanged;
+
+        private void OnNextBallIndexChanged(int index)
+        {
+            if (everyBallData.TryGetBallData(index, out var ball))
+            {
+                _nextBallImage.sprite = ball.BallSprite;
+                gameObject.transform.localScale = ball.BallSize;
+            }
+            else
+            {
+                gameObject.transform.localScale = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIScore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Data;
+using Data.Variables;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class UIScore : MonoBehaviour
+    {
+        [SerializeField] private ObservableIntValue score;
+        private TMP_Text _scoreText;
+        
+        private void Awake() => _scoreText = GetComponent<TMP_Text>();
+
+        private void OnEnable() => score.OnValueChanged += OnScoreChanged;
+
+        private void OnDisable() => score.OnValueChanged -= OnScoreChanged;
+
+        private void OnScoreChanged(int scoreVal) => _scoreText.text = scoreVal.ToString();
+    }
+}
