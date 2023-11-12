7715e10e4a6cfcdc1f11123f8510b3bb5d4096c6
alzseven <jhp7140@gmail.com>
2023-11-13 04:31:09 +0900

    Added AudioManager & Timer

---
+++ Assets/Scripts/SimpleAudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Data;
+using UnityEngine;
+
+public class SimpleAudioManager : MonoBehaviour
+{
+    [SerializeField] private SfxEventPublisher onThrowBallPublisher;
+    [SerializeField] private SfxEventPublisher onMergeBallPublisher;
+    [SerializeField] private AudioSource sfxSource;
+
+    private void OnEnable()
+    {
+        onThrowBallPublisher.OnSfxEventOccured += PlayOnShotClip;
+        onMergeBallPublisher.OnSfxEventOccured += PlayOnShotClip;
+    }
+
+    private void OnDisable()
+    {
+        onThrowBallPublisher.OnSfxEventOccured -= PlayOnShotClip;
+        onMergeBallPublisher.OnSfxEventOccured -= PlayOnShotClip;
+    }
+
+    private void PlayOnShotClip(AudioClip clip) => sfxSource.PlayOneShot(clip);
+}
---
+++ Assets/Scripts/SimpleTimer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Data.Variables;
+using UnityEngine;
+
+public class SimpleTimer : MonoBehaviour
+{ 
+    public ObservableFloatValue gameTime;
+
+    private void Update() => gameTime.Value += Time.deltaTime;
+}
