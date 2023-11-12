fd872ddced0d6a79e2d52933c4ffb2c97099c7fc
alzseven <jhp7140@gmail.com>
2023-11-13 03:59:32 +0900

    Create static variables
    - Current & Next ball Index
    - Score
    - GameTime

---
+++ Assets/Scripts/Data/Variables/ObservableFloatValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Data.Variables
+{
+    [CreateAssetMenu(fileName = "ObservableFloatValue", menuName = "Data/Variables/ObservableFloatValue", order = 1)]
+    public class ObservableFloatValue : ScriptableObject
+    {
+        [SerializeField] private float value;
+        public event Action<float> OnValueChanged;
+        
+        public float Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                OnValueChanged?.Invoke(value);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Data/Variables/ObservableIntValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Data.Variables
+{
+    [CreateAssetMenu(fileName = "ObservableIntValue", menuName = "Data/Variables/ObservableIntValue", order = 0)]
+    public class ObservableIntValue : ScriptableObject
+    {
+        [SerializeField] private int value;
+        public event Action<int> OnValueChanged;
+        
+        public int Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                OnValueChanged?.Invoke(value);
+            }
+        }
+    }
+}
