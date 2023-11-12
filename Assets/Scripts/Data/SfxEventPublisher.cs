ffaccd08f487aa6836ce6a0d21a199b3a1782a3c
alzseven <jhp7140@gmail.com>
2023-11-13 04:29:02 +0900

    Create static event publishers
    - onFire
    - onThrowball, onMergeBall (sfx)

---
+++ Assets/Scripts/Data/SfxEventPublisher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "SfxEventPublisher", menuName = "Data/Event/SfxEventPublisher", order = 1)]
+    public class SfxEventPublisher : ScriptableObject
+    {
+        [SerializeField] private AudioClip sfxClip;
+        public UnityAction<AudioClip> OnSfxEventOccured;
+        
+        public void PublishMessage()
+        {
+            OnSfxEventOccured?.Invoke(sfxClip);
+        }
+    }
+}
---
+++ Assets/Scripts/Data/SimpleEventPublisher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "SimpleEventPublisher", menuName = "Data/Event/SimpleEventPublisher", order = 0)]
+    public class SimpleEventPublisher : ScriptableObject
+    {
+        public UnityAction OnEventOccured;
+
+        public void PublishMessage()
+        {
+            OnEventOccured?.Invoke();
+        }
+    }
+}
